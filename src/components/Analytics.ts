bfde28defb3cfc1f7c7f153b8a9dc185c6fff37f
Augustin Verissimo <augustin.verissimo@epitech.eu>
2025-04-07 15:06:52 +0200

    feat/add vercel analytics

--- src/components/Analytics.ts
+++
@@ -1,6 +0,0 @@
-import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
-import React from 'react';
-
-export function Analytics() {
-  return React.createElement(VercelAnalytics);
-}
